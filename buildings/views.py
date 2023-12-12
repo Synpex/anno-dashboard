3758bfecfe6121caecfda1198857713dcf507d7c
pnuam <73306987+pnuam@users.noreply.github.com>
2023-12-12 10:49:49 +0100

    Add map to buildings.html

--- buildings/views.py
+++ buildings/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def buildings_view(request):
     # You can add your logic here to pass context to your dashboard template
+    # mapbox_access_token = 'pk.eyJ1IjoicG51YW0iLCJhIjoiY2xxMHlnZWNuMDM1cjJpbzExcTlqaGJ6NCJ9._P7uUlMRTrZCiFyzQCGTjw'
     context = {
         'section': 'buildings',
         # Add more context variables here
