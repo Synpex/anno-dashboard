7ab2ae6d87bd49328e194dbc15ccb99c64f33fbf
pnuam <73306987+pnuam@users.noreply.github.com>
2024-01-01 15:57:01 +0100

    Add restructured HTML templates

--- buildings/urls.py
+++ buildings/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import buildings_view, import_detail_view, import_images_view, import_timeline_view, import_position_view, import_review_view
+from .views import buildings_view, import_detail_view, import_images_view, import_timeline_view, import_position_view, import_review_view, import_audioguides_view
 
 urlpatterns = [
 path('', buildings_view, name='buildings'),
@@ -7,4 +7,8 @@ path('import/detail', import_detail_view, name='import'),
 path('import/images', import_images_view, name='import'),
 path('import/timeline', import_timeline_view, name='import'),
 path('import/position', import_position_view, name='import'),
-path('import/review', import_review_view, name='import'),]
+path('import/review', import_review_view, name='import'),
+path('import/audioguides', import_audioguides_view, name='import'),
+
+
+]
