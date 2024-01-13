dc9fe8a8d3b0f388dd303efc6dc4bdb6072b6377
sebastian-preis <preis@computer.org>
2024-01-13 20:44:54 +0100

    Change log level of NoMigrationRouter

--- annodashboard/router.py
+++ annodashboard/router.py
@@ -14,9 +14,9 @@ class NoMigrateRouter:
         Attempts to read 'buildings' models go to a different database.
         """
         if model._meta.app_label == 'buildings':
-            logger.info(f"Routing READ operation for {model._meta.model_name} to 'buildings' database.")
+            logger.debug(f"Routing READ operation for {model._meta.model_name} to 'buildings' database.")
             return 'buildings'
-        logger.info(f"Routing READ operation for {model._meta.model_name} to default database.")
+        logger.debug(f"Routing READ operation for {model._meta.model_name} to default database.")
         return None
 
     def db_for_write(self, model, **hints):
@@ -24,9 +24,9 @@ class NoMigrateRouter:
         Attempts to write 'buildings' models go to a different database.
         """
         if model._meta.app_label == 'buildings':
-            logger.info(f"Routing WRITE operation for {model._meta.model_name} to 'buildings' database.")
+            logger.debug(f"Routing WRITE operation for {model._meta.model_name} to 'buildings' database.")
             return 'buildings'
-        logger.info(f"Routing WRITE operation for {model._meta.model_name} to default database.")
+        logger.debug(f"Routing WRITE operation for {model._meta.model_name} to default database.")
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
@@ -34,5 +34,5 @@ class NoMigrateRouter:
         Do not allow migrations for the 'buildings' app.
         """
         migrate_decision = 'ALLOW' if app_label != 'buildings' else 'PREVENT'
-        logger.info(f"Migrate Decision: {migrate_decision}. DB: {db}, App: {app_label}, Model: {model_name}")
+        logger.debug(f"Migrate Decision: {migrate_decision}. DB: {db}, App: {app_label}, Model: {model_name}")
         return app_label != 'buildings'
